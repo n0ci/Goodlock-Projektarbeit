74919c214438cd716c56c88ee4fa008a42e42738
Fabio Nocera <nofa1011@h-ka.de>
2021-12-04 20:17:15 +0100

    added doc for MyMutex and MyThread

--- src/MyThread.h
+++ src/MyThread.h
@@ -1,13 +1,22 @@
 #include <thread>
 
-
+/**
+ * Die Klasse MyThread stellt zu einem Thread eine Thread ID (mid) zur Verfügung.
+ */
 class MyThread{
 public:
-    MyThread() = default;
-
     int tid = -1;
     std::thread thread;
 
+    /**
+     * Erstellt ein Objekt MyThread.
+     */
+    MyThread()= default;
+
+    /**
+     * Übergibt einem Objekt MyThread eine tid.
+     * @param mid Thread Id.
+     */
     explicit MyThread(int tid){
         this->tid = tid;
     }
